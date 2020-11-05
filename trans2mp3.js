399120e45c49d216ce3ca6a6eff0f8d5a0cc8af7
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-11-05 14:33:54 +0700

    Add playing controller

---
+++ trans2mp3.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+const { spawn } = require("child_process");
+const path = require("path");
+const app = new express();
+
+const router = new express.Router();
+
+const config = {
+  STREAM: "rtmp://0.0.0.0/live",
+};
+
+router.get("/live/:key", (req, res) => {
+  const { key } = req.params;
+
+  let command = ffmpeg(`${config.STREAM}${key}/${key}`)
+    .addInputOption("-re")
+    .addOutputOption("-f mp3");
+
+  let ffstream = command.pipe();
+  res.set("content-type", "audio/mp3");
+  res.set("accept-ranges", "bytes");
+
+  ffstream.on("data", function (chunk) {
+    res.write(chunk);
+  });
+});
+
+module.exports = router;
