e19b93ff7aecf3b625ac3a28fe0346b712289326
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-11-10 17:10:52 +0700

    Remove close listener

--- trans2mp3.js
+++ trans2mp3.js
@@ -15,33 +15,6 @@ app.get("/live/:key", (req, res) => {
   let ffstream = conn.stream;
 
   ffstream.pipe(res);
-
-  // setTimeout(function () {
-  //   if (!started) {
-  //     console.log(`Connection timeout: ${conn.channel}`);
-  //     service.instance().removeChannel(conn.channel);
-  //     res.status(404).send({ message: "timeout" });
-  //   }
-  // }, 30000);
-
-  // conn.command.on("end", function (stdout, stderr) {
-  //   service.instance().removeChannel(conn.channel);
-  //   console.log(`Close Ended channel: ${conn.channel}`);
-  // });
-  // conn.command.on("error", function (err, stdout, stderr) {
-  //   service.instance().removeChannel(conn.channel);
-  //   console.log(`Close Error channel: ${conn.channel}`);
-  // });
-
-  ffstream.on("close", function () {
-    service.instance().removeChannel(conn.channel);
-    console.log(`Close channel: ${conn.channel}`);
-  });
-
-  // ffstream.on("error", function () {
-  //   service.instance().removeChannel(conn.channel);
-  //   console.log(`Error and remove channel: ${conn.channel}`);
-  // });
 });
 
 module.exports = app;
