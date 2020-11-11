eed9449277871cdfa3823c78b4737636e9792793
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-11-11 12:00:29 +0700

    Enable rtmp

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const config = {
         app: "live",
         hls: true,
         hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        rtmp: true,
+        rtmpApp: "live2",
         dash: true,
         dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
       },
